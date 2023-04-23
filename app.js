fe8ea7b04af8749860b6fb0a524ef9f2d522cfee
DzianisPrakharchuk <denprohorchuk@gmail.com>
2023-04-23 12:33:43 +0300

    refactor: formatting according to rule 3.4 in html-and-css.md

--- app.js
+++ app.js
@@ -10,7 +10,7 @@
 
 var taskInput=document.getElementById("new-task");//Add a new task.
 var addButton=document.getElementsByTagName("button")[0];//first button
-var incompleteTaskHolder=document.getElementById("incomplete-tasks");//ul of #incompleteTasks
+var incompleteTaskHolder=document.getElementById("incomplete-tasks");//ul of #incomplete-tasks
 var completedTasksHolder=document.getElementById("completed-tasks");//completed-tasks
 
 
@@ -33,7 +33,13 @@ var createNewTaskElement=function(taskString){
     var deleteButtonImg=document.createElement("img");//delete button image
 
     label.innerText=taskString;
-    label.className='task';
+    label.className='task task-info';
+
+    listItem.className = 'list';
+
+    checkBox.className = 'checkbox';
+
+    deleteButtonImg.className = 'delete-img';
 
     //Each elements, needs appending
     checkBox.type="checkbox";
